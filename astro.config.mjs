db740f7006600ee78f54d95fe41be1c018c35e27
Vladimir JV <ivladimirjuarez@outlook.com>
2024-01-18 16:52:27 -0600

    feat: add deployment config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-portfolio-uzair.vercel.app",
+  site: 'https://vladimirjv.github.io',
+  base: '/resume',
   integrations: [
     tailwind(),
     sitemap({
